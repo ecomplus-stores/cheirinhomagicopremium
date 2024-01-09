c194c950f7658d825d1d2e365dc9b52e8105c403
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2024-01-09 18:24:46 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,3 +1,4 @@
+import EcomSearch from '@ecomplus/search-engine'
 document.getElementById('c-64f1c5712cd6b65959370481').href = '/home-spray-premium-60-ml';
 document.getElementById('c-64f1d7972cd6b65959372789').href = '/home-spray-premium-500-ml';
 document.getElementById('c-64f1dde62cd6b65959373aa4').href = '/agua-perfumada-premium-500-ml';
